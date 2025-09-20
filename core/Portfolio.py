c974ecf58ed8e942bf01f8ea697d18359254dd13
Ethan <ethanglloo@gmail.com>
2025-09-20 11:14:14 -0700

    A few changes to portfolio class

--- core/Portfolio.py
+++ core/Portfolio.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import Dict
 
 
 class Portfolio:
@@ -6,23 +7,25 @@ class Portfolio:
     def __init__(self, name, currency, positions):
         self.name = name
         self.currency = currency
-        self.positions = {} # {ticker: amount held in $}
+        self.positions = Dict[str:float] # [ticker: amount held in $]
 
     def add_positons(self, value, ticker):
-        self.positions[ticker] = value
-        return self.positions
+        self.positions[ticker] = self.positions.get(ticker, 0.0) + value
     
     def delete_position(self, ticker):
+        """Removes postion from portfolio"""
         if ticker in self.positions:
             del self.positions[ticker]
 
     def portfolio_val(self):
-        value = 0
-        for ticker in self.positions:
-            value += self.postions[ticker]
-        return value
-
+        """calculates the sum of the amount held in each position to calculate full portfolio value"""
+        return sum(self.positions.values())
     
-    
-    
+    def weights(self):
+        """create a dictionary for tickers and their weighting in the portfolio"""
+        
+        total = self.portfolio_val()
+        weights = {ticker: value / total for ticker, value in self.positions.items()}
+        
+        return weights
 
