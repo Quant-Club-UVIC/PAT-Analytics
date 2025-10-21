56e7bdb953f7095b68680725e22f728df505b388
Ethan <ethanglloo@gmail.com>
2025-10-20 19:29:43 -0700

    implemented cvar

--- pat_analytics/analytics/risk/var.py
+++ pat_analytics/analytics/risk/var.py
@@ -23,5 +23,13 @@ class VaRAnalytics(RiskBase):
         Implementaton of 
         Conditional Value at Risk
         """
-        return "cvar!"
+
+        net_returns : pd.Series = 1 - self.returns # calculate net returns
+
+        threshold = np.percentile(net_returns, (1 - self.level) * 100) # This is var or the threshold
+
+        tail_losses = net_returns[net_returns >= threshold] # create a variable for all of the losses beyond var
+
+        return tail_losses.mean() # calculate cvar the mean of the potential losses
+    
     
